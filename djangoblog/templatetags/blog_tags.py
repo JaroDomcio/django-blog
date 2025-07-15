642c5c5732d4a16502094b574c588d3cc63b4f95
JaroDomcio <281053@student.pwr.edu.pl>
2025-07-15 11:10:40 +0200

    Add another tag (most commented posts)

--- djangoblog/templatetags/blog_tags.py
+++ djangoblog/templatetags/blog_tags.py
@@ -8,12 +8,13 @@ register = template.Library()
 def total_posts():
     return Post.objects.filter(status = Post.Status.PUBLISHED).count()
 
-@register.inclusion_tag('latest_posts.html')
+@register.inclusion_tag('post_enumerating_template.html')
 def show_latest_posts(count=5):
     latest_posts = Post.objects.filter(status = Post.Status.PUBLISHED).order_by('-created_date')[:count]
-    return {'latest_posts':latest_posts}
+    return {'posts':latest_posts}
 
 
-# @register.simple_tag
-# def get_most_commented_posts(count=5):
-#     return Post.objects.filter(status = Post.Status.PUBLISHED).order_by('-created_date')[:count]
+@register.inclusion_tag('post_enumerating_template.html')
+def show_most_commented_posts(count=5):
+    most_commented_posts = Post.objects.filter(status = Post.Status.PUBLISHED).annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
+    return {'posts':most_commented_posts}
